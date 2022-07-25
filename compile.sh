f7a29188195762338c09a73e4be9cb10975dcece
Kārlis Gaigals <karlis@nic.lv>
2022-07-25 13:41:38 +0300

    Removed commented line

--- compile.sh
+++ compile.sh
@@ -5,7 +5,6 @@ WASM_DIR=$PROJECT_DIR/wasm
 STATIC_DIR=$PROJECT_DIR/static
 
 cd $WASM_DIR
-#cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" $STATIC_DIR
 
 if [ "$TINYGO" == "FALSE" ]; then
   GOOS=js GOARCH=wasm go build -o $STATIC_DIR/main.wasm
